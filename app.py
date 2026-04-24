43256b1bd2f9c31922e0eb1c3bf0bfcd077d5fba
Suprith-3 <suprithnagaraj3@gmail.com>
2026-04-25 00:16:23 +0530

    Project Updated: 2026-04-25 06:23:17 | Fixed all missing icons and weather widget

--- app.py
+++ app.py
@@ -212,13 +212,13 @@ if __name__ == '__main__':
 def add_security_headers(response):
     # Content Security Policy (Allows necessary scripts and styles)
     response.headers['Content-Security-Policy'] = (
-        "default-src 'self'; "
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://cdn.tailwindcss.com https://checkout.razorpay.com https://translate.google.com https://translate.googleapis.com https://kit.fontawesome.com; "
-        "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com https://unpkg.com https://translate.googleapis.com https://cdnjs.cloudflare.com https://ka-f.fontawesome.com; "
-        "img-src 'self' data: https:; "
-        "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://ka-f.fontawesome.com; "
+        "default-src 'self' https:; "
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; "
+        "style-src 'self' 'unsafe-inline' https:; "
+        "img-src 'self' data: https: http://openweathermap.org https://openweathermap.org; "
+        "font-src 'self' data: https:; "
         "frame-src 'self' https://checkout.razorpay.com; "
-        "connect-src 'self' https://api.razorpay.com https://lumberjack.razorpay.com https://translate.googleapis.com https://ka-f.fontawesome.com;"
+        "connect-src 'self' https:;"
     )
     
     # HSTS (Forces HTTPS - 1 year)
